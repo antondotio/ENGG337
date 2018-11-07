b667ec1f600bc26e86624a8e2a482cb1f868fcde
Anton Santos <43733827+antondotio@users.noreply.github.com>
2018-11-06 22:42:16 -0700

    lab8a

--- lab8Clock.cpp
+++ lab8Clock.cpp
@@ -2,3 +2,97 @@
 #include <stdlib.h>
 #include "lab8Clock.h"
 using namespace std;
+
+int Clock::get_hour() const { return hour; }
+void Clock::set_hour(int h){ 
+    if(h < 23 && h >= 0)
+        hour = h;
+}
+
+int Clock::get_minute() const { return minute; }
+void Clock::set_minute(int m){ 
+    if(m < 59 && m >= 0)
+        minute = m; 
+}
+
+int Clock::get_second() const { return second; }
+void Clock::set_second(int s){ 
+    if(s < 59 && s >= 0)
+        second = s;
+}
+
+Clock::Clock(): hour(0), minute(0), second(0) { };
+
+Clock::Clock(int s){
+    if(s < 0){
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }else{
+        sec_to_hms(s);
+    }
+}
+
+Clock::Clock(int h, int m, int s){
+    if(h > 23 || h < 0 || m > 59 || m < 0 || s > 59 || s < 0){
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }else{
+        hour = h;
+        minute = m;
+        second = s;
+    }
+}
+
+void Clock::increment(){
+    second++;
+    if(second > 59){
+        second = 0;
+        minute++;
+    }
+    if(minute > 59){
+        minute = 0;
+        hour++;
+    }
+    if(hour > 23){
+        hour = 0;
+    }
+}
+
+void Clock::decrement(){
+    second--;
+    if(second < 0){
+        second = 59;
+        minute--;
+    }
+    if(minute < 0){
+        minute = 59;
+        hour--;
+    }
+    if(hour < 0){
+        hour = 23;
+    }
+}
+
+void Clock::add_seconds(int s){
+    if (s >= 0){
+        int t;
+        t = hms_to_sec() + s;
+        sec_to_hms(t);
+    }
+}
+
+int Clock::hms_to_sec(){
+    int t = hour * 3600 + minute * 60 + second;
+    return t;
+}
+
+void Clock::sec_to_hms(int s){
+    hour = s / 3600;
+    minute = (s % 3600) / 60;
+    second = (s % 3600) % 60;
+    if(hour > 23){
+        hour = hour % 24;
+    }
+}
--- lab8Clock.h
+++ lab8Clock.h
@@ -1,2 +1,33 @@
+
 #ifndef lab8clock_h
 #define lab8clock_h
+
+class Clock{
+public:
+    Clock();
+    Clock(int s);
+    Clock(int h, int m, int s);
+
+    int get_hour() const;
+    void set_hour(int h);
+
+    int get_minute() const;
+    void set_minute(int m);
+
+    int get_second() const;
+    void set_second(int s);
+
+    void increment();
+    void decrement();
+    void add_seconds(int s);
+
+private:
+    int hour;
+    int minute;
+    int second;
+
+    int hms_to_sec();
+    void sec_to_hms(int s);
+};
+
+#endif
